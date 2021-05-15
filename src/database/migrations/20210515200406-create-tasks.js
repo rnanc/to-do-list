88ce0374d476a8307dcaeecaf182374b2fcb8e70
Renan <renanfigcarneiro@gmail.com>
2021-05-15 17:58:35 -0300

    feat: users, projects and tasks migrations

---
+++ src/database/migrations/20210515200406-create-tasks.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('tasks', {
+      id: { 
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      project_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { 
+          model: 'projects', key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      created_at : {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at : {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('tasks')
+  }
+};
