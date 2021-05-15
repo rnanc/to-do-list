9f17c8aa838d4c4d8ce757f7c1bb4ab5e1e0179d
Renan <renanfigcarneiro@gmail.com>
2021-05-15 18:29:39 -0300

    feat: added tasks route

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const { ensureAuthenticate } = require("./middlewares/ensureAuthenticated")
 const UserController = require("./controllers/UserController")
 const SessionController = require("./controllers/SessionController")
 const ProjectController = require("./controllers/ProjectController")
+const TaskController = require("./controllers/TaskController")
 
 const routes = express.Router()
 
@@ -18,5 +19,12 @@ routes.post('/projects', ensureAuthenticate,ProjectController.createProject)
 routes.get('/projects', ensureAuthenticate,ProjectController.findAllProjects)
 routes.delete('/projects/:id', ensureAuthenticate,ProjectController.deleteProject)
 routes.put('/projects', ensureAuthenticate,ProjectController.editProject)
+
+routes.post('/tasks', ensureAuthenticate, TaskController.createTask)
+routes.get('/tasks/:project_id', ensureAuthenticate, TaskController.findAllTasks)
+routes.delete('/tasks/:id', ensureAuthenticate, TaskController.deleteTask)
+routes.post('/tasks/finish/:id', ensureAuthenticate, TaskController.finishTask)
+routes.post('/tasks/reopen/:id', ensureAuthenticate, TaskController.reOpenTask)
+routes.put('/tasks', ensureAuthenticate, TaskController.editTask)
 
 module.exports = routes
